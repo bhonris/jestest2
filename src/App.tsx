1149fa7f02e79faab0fac351393fb5c0505d7357
Bhon Bunnag <bhonris@gmail.com>
2021-06-02 21:05:37 -0700

    add additional stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,17 @@
-import React, {  useState } from 'react';
+import React from 'react';
+import logo from './logo.png';
+import './App.css';
+import PalindromeChecker from './components/PalindromeChecker';
 
-
-export const isPalindrome = (value: string): boolean => {
-  return value.split("").reverse().join("") === value
-}
-
-export enum Result {
-  yes = "Yes",
-  no = "Nope"
-}
-
-
-const sleep = async (ms: number) => {
-  return new Promise(resolve => setTimeout(resolve, ms))
-}
-
-function PalindromeChecker() {
-  const [inputValue, setInputValue] = useState('')
-  const [resultValue, setResultValue] = useState('')
-  
-  const displayResult = async (event: React.MouseEvent): Promise<void> => {
-    setResultValue('calculating...')
-    const result = isPalindrome(inputValue) ? Result.yes : Result.no
-    setResultValue(result)
-
-  }  
-
+function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Is it a palindrome? </h1>
-        <input type="text" id="name" name="name" value={inputValue} 
-          onChange={e => setInputValue(e.target.value)} required/>
-        <input onClick={displayResult} type="button" value="Submit"/>
-        <h3>{resultValue}</h3>
+        <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <PalindromeChecker/>
     </div>
-  )
-  
+  );
 }
 
-export default PalindromeChecker
+export default App;
---
+++ src/components/PalindromeChecker.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+
+export const generateFizzBuzz = (value: string): number[] => {
+  throw "Function not implemented"
+}
+
+export enum Result {
+  yes = "Yes",
+  no = "Nope"
+}
+
+
+const sleep = async (ms: number) => {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+function PalindromeChecker() {
+  const [inputValue, setInputValue] = useState('')
+  const [resultValue, setResultValue] = useState<number[]>([])
+  
+  const displayResult = async (event: React.MouseEvent): Promise<void> => {
+    const result = generateFizzBuzz(inputValue);
+    setResultValue(result)
+
+  }  
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Fizz Buzz Generator </h1>
+        <input type="text" id="name" name="name" value={inputValue} 
+          onChange={e => setInputValue(e.target.value)} required/>
+        <input onClick={displayResult} type="button" value="Submit"/>
+        <h3>{resultValue}</h3>
+      </header>
+    </div>
+  )
+  
+}
+
+export default PalindromeChecker
